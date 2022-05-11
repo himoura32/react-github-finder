e9475ebb9aa1fc669af27856b80f6e4bb68e316f
himoura32 <shawn.ahearn@outlook.com>
2022-05-11 10:54:45 -0400

    search and get to actions file/refactored w/axios

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,16 +5,22 @@ import Spinner from '../components/layout/Spinner';
 import GithubContext from '../context/github/GithubContext';
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa';
 import RepoList from '../components/repos/RepoList';
+import { getUserAndRepos } from '../context/github/GithubActions';
 
 function User() {
-  const { getUser, user, loading, getUserRepos, repos } = useContext(GithubContext);
+  const { user, loading, repos, dispatch } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
-    getUser(params.login);
-    getUserRepos(params.login);
-  }, []);
+    dispatch({ type: 'SET_LOADING' });
+    const getUserData = async () => {
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: 'GET_USER_AND_REPOS', payload: userData });
+    };
+
+    getUserData();
+  }, [dispatch, params.login]);
 
   const {
     name,
@@ -116,56 +122,48 @@ function User() {
             </div>
           </div>
         </div>
-        <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaUsers className='text-3xl md:text-5xl'/>
+        <div className='w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats'>
+          <div className='stat'>
+            <div className='stat-figure text-secondary'>
+              <FaUsers className='text-3xl md:text-5xl' />
             </div>
-            <div className="stat-title pr-5">
-              Followers
-            </div>
-            <div className="stat-value pr-5 text-3xl md:text-4xl">
+            <div className='stat-title pr-5'>Followers</div>
+            <div className='stat-value pr-5 text-3xl md:text-4xl'>
               {followers}
             </div>
           </div>
 
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaUserFriends className='text-3xl md:text-5xl'/>
+          <div className='stat'>
+            <div className='stat-figure text-secondary'>
+              <FaUserFriends className='text-3xl md:text-5xl' />
             </div>
-            <div className="stat-title pr-5">
-              Following
-            </div>
-            <div className="stat-value pr-5 text-3xl md:text-4xl">
+            <div className='stat-title pr-5'>Following</div>
+            <div className='stat-value pr-5 text-3xl md:text-4xl'>
               {following}
             </div>
           </div>
 
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaCodepen className='text-3xl md:text-5xl'/>
+          <div className='stat'>
+            <div className='stat-figure text-secondary'>
+              <FaCodepen className='text-3xl md:text-5xl' />
             </div>
-            <div className="stat-title pr-5">
-              Public Repos
-            </div>
-            <div className="stat-value pr-5 text-3xl md:text-4xl">
+            <div className='stat-title pr-5'>Public Repos</div>
+            <div className='stat-value pr-5 text-3xl md:text-4xl'>
               {public_repos}
             </div>
           </div>
 
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaStore className='text-3xl md:text-5xl'/>
+          <div className='stat'>
+            <div className='stat-figure text-secondary'>
+              <FaStore className='text-3xl md:text-5xl' />
             </div>
-            <div className="stat-title pr-5">
-              Public Gists
-            </div>
-            <div className="stat-value pr-5 text-3xl md:text-4xl">
+            <div className='stat-title pr-5'>Public Gists</div>
+            <div className='stat-value pr-5 text-3xl md:text-4xl'>
               {public_gists}
             </div>
           </div>
         </div>
-        <RepoList repos={repos}/>
+        <RepoList repos={repos} />
       </div>
     </>
   );
